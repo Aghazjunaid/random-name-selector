5626ec71cf00c2d4c6f28a73bafd5b1d2c061845
unknown <aghazjunaid96@gmail.com>
2022-03-23 10:28:27 +0530

    misc upd

---
+++ main.js
@@ -0,0 +1,21 @@
+let data = [
+    "Aghaz",
+    "Ayush",
+    "Arwaz",
+    "Arman",
+    "Rohit",
+    "Bilal",
+    "Pranay",
+    "Farhan",
+    "Aaliya",
+    "Raihan",
+    "Aquib",
+    "Faiyaz",
+    "Tausif",
+    "Silpy",
+    "Rishabh"
+]
+
+let count = document.getElementById('countLength')
+console.log(count)
+count.innerText = data.length
