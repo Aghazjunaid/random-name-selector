d5e04b76ff8ad1d20f3115e410c20f77db98c90d
unknown <aghazjunaid96@gmail.com>
2022-03-23 11:59:40 +0530

    getting random data

--- main.js
+++ main.js
@@ -16,6 +16,37 @@ let data = [
     "Rishabh"
 ]
 
-let count = document.getElementById('countLength')
-console.log(count)
-count.innerText = data.length
+
+let newArr = []
+function displayLength(){
+    let count = document.getElementById('countLength')
+    let data1 = localStorage.getItem('list')
+    count.innerText = data1.split(',').length
+}
+
+function maintainList(i){
+    newArr.push(data[i])
+    localStorage.setItem('newList',newArr)
+    data.splice(i,1)
+    localStorage.setItem("list",data)
+    displayLength()
+}
+
+function getRandomData(){
+    let index = Math.floor(Math.random()*data.length)
+    let selectedData = data[index];
+    if(selectedData){
+        let ele = document.querySelector('#selected-name');
+        ele.innerText = selectedData
+        maintainList(index)
+    }else{
+        let ele = document.querySelector('#selected-name');
+        ele.innerText = "Array is Empty"
+        let count = document.getElementById('countLength')
+        count.innerText = 0
+    }
+    
+}
+
+let btn = document.querySelector("#randomBtn")
+btn.addEventListener('click',getRandomData)
